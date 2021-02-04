5dd72a39c12277d5e9debb3d15f1691ec3a831b4
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-04 23:54:45 +0000

    dynamically updating prices / sale alert on product pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
+    @active_sale = Sale.active.first
   end
 
 end
