9f00c5e6e8a63ee6c8079c330e7a1cd5e7721f53
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-10 19:21:19 +0000

    fixing bugs and finishing implementation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,7 @@
 class UserMailer < ApplicationMailer
-  default from: 'no-reply@jungle.com'
 
-  def order_email
-    @user = params[:user]
-    @order = params[:order]
-    mail(to: @user.email, subject: "Thank you for your order #{@order.id}")
+  def order_email(order)
+    @order = order
+    mail(to: @order.email, subject: "Thank you for your order ##{@order.id}")
   end
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def order_email
+    UserMailer.order_email(Order.last)
+  end
 end
