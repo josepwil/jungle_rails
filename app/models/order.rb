6bbf347693bae056d276db878d756c58f52da35a
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-10 22:50:48 +0000

    implementing inventory adjustment logic

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,14 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  after_create do |order|
+    items = LineItem.where(order_id: id).all
+
+    items.each do |item|
+      product = Product.find(item.product_id)
+      newQuantity = product.quantity.to_i - item.quantity
+      product.update(quantity: newQuantity)
+    end
+  end
+
 end
